491c5a22a3c283f08a3790d2c0e4d9a6ac3a3c85
albertorm1 <albertorodriguezmuelas@outlook.com>
2024-05-10 10:48:05 +0200

    Add -> Fouls subclasses

---
+++ src/main/java/es/alrodmue/model/fouls/TechnicalFoul.java
@@ -0,0 +1,34 @@
+
+package es.alrodmue.model.fouls;
+
+import es.alrodmue.model.exceptions.FoulInvalidDataException;
+import es.alrodmue.model.matches.Match;
+import es.alrodmue.model.players.Player;
+
+/**
+ * Clase para faltas personales
+ * @author Alberto Rodriguez Muelas
+ */
+public class TechnicalFoul extends Foul {
+    
+    /**
+     * Constructor, que crea la falta en función a un jugador y un partido.
+     * @param player Jugador que comete la falta.
+     * @param match Partido en el que se comete la falta.
+     * @throws FoulInvalidDataException Excepción que se produce si alguno de los parámetros no son válidos. Contiene un mensaje de error
+     * entendible por el usuario.
+     */
+    public TechnicalFoul(Player player, Match match) throws FoulInvalidDataException {
+        super(player, match);
+    }
+
+    /**
+     * Método que indica el tipo de la falta.
+     * @returns String con el tipo de la falta.
+     */
+    @Override
+    public String getType() {
+        return "Falta técnica";
+    }
+
+}
