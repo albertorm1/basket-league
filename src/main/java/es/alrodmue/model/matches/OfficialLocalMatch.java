0297d4feec1defd6a8054c2dbd8755276b6f3aa1
albertorm1 <albertorodriguezmuelas@outlook.com>
2024-05-10 12:25:49 +0200

    Add -> Subclasses for Match abstract class

---
+++ src/main/java/es/alrodmue/model/matches/OfficialLocalMatch.java
@@ -0,0 +1,40 @@
+package es.alrodmue.model.matches;
+
+import java.time.LocalDate;
+
+import es.alrodmue.model.exceptions.MatchInvalidDataException;
+
+/**
+ * Clase para partidos oficiales jugando como equipo local.
+ * @author Alberto Rodriguez Muelas
+ */
+public class OfficialLocalMatch extends Match {
+    
+    /**
+     * Constructor del partido, que asigna automáticamente los puntos a cada equipo tras calcularlos de forma aleatoria, y genera las faltas cometidas.
+     * @param date Fecha del partido
+     * @throws MatchInvalidDataException Excepción que indica si algún dato del partido no es válido. El mensaje de la excepción contiene un mensaje
+     * de error entendible por el usuario.
+     */
+    public OfficialLocalMatch(LocalDate date) throws MatchInvalidDataException {
+        super(date);
+    }
+
+    /**
+     * Método que decide que equipo gana.
+     * @returns Valor booleano que indica si gana el equipo propio (true) o el rival (false)
+     */
+    @Override
+    protected boolean isOwnTeamWinner() {
+        return Math.random() * 10 < 5;
+    }
+
+    /**
+     * Método que devuelve el tipo de partido.
+     * @returns String con el tipo de partido.
+     */
+    @Override
+    public String getType() {
+        return "Oficial (local)";
+    }
+}
