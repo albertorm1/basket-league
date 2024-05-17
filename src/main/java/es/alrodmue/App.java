21822742694c48925dd71937c9c51ccde1c85177
Alberto Rodriguez <albertorodriguezmuelas@outlook.com>
2024-05-17 09:17:00 +0200

    Add -> support for modals

--- src/main/java/es/alrodmue/App.java
+++ src/main/java/es/alrodmue/App.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,6 +15,7 @@ import java.io.IOException;
 public class App extends Application {
 
     private static Scene scene;
+    private static Stage modalStage;
 
     @Override
     public void start(Stage stage) throws IOException {
@@ -29,6 +31,21 @@ public class App extends Application {
     private static Parent loadFXML(String fxml) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
         return fxmlLoader.load();
+    }
+
+    public static void showModal(String fxml, String title) throws IOException {
+        modalStage = new Stage();
+        Scene scene = new Scene(loadFXML(fxml));
+        modalStage.setScene(scene);
+        modalStage.setTitle(title);
+        modalStage.setResizable(false);
+        modalStage.initModality(Modality.APPLICATION_MODAL);
+        modalStage.show();
+    }
+
+    public static void closeModal() {
+        modalStage.close();
+        modalStage = null;
     }
 
     public static void main(String[] args) {
